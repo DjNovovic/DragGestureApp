c066468d56470fc7c58faca21ec88ca389e90bf0
DjNovovic <djordjijenovovic@gmail.com>
2021-09-04 21:19:26 +0200

    Initial Commit

---
+++ DragGestureApp/DragGestureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureAppApp.swift
+//  DragGestureApp
+//
+//  Created by Đorđije Novović on 4.9.21..
+//
+
+import SwiftUI
+
+@main
+struct DragGestureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
